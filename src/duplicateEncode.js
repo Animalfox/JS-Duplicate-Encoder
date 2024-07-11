3446a35dd32db0090a28a32f2e6038ff3aa895de
Animalfox <a72e@outlook.com>
2024-07-11 22:09:32 +0300

    Solve exercise

--- src/duplicateEncode.js
+++ src/duplicateEncode.js
@@ -1,4 +1,29 @@
+/**
+ * Encode a word where each character is replaced by '(' if it appears only once,
+ * or ')' if it appears more than once.
+ * 
+ * @param {string} word - The input word to be encoded.
+ * @returns {string} - The encoded string where '(' represents non-duplicated characters
+ *                     and ')' represents duplicated characters.
+ * @author Animalfox
+ * @link https://github.com/Animalfox/JS-Duplicate-Encoder
+ */
 export default function duplicateEncode(word){
-    // ...
-    return '';
+    // Split input word to array with it characters
+    const charArr = word.toLowerCase().split('');
+
+    // Encode array by using map function
+    const encodedArr = charArr.map(char => {
+
+        // Detect duplication
+        const isDuplicate = charArr.filter(l => l == char).length > 1;
+
+        // Return encoded symbol
+        return isDuplicate ? ")" : "(";
+    });
+
+    // Convert encoded array back into string
+    const output = encodedArr.join('');
+
+    return output;
 }
